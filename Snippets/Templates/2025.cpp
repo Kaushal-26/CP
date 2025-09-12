64eac4e8ce3336303faec9007c06f9deb1dc64ec
Kaushal_26 <kaushalsolanki260102@gmail.com>
2025-09-13 00:58:16 +0530

    fix: Minor indentation fix in 2025.cpp template

--- Snippets/Templates/2025.cpp
+++ Snippets/Templates/2025.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 
 int main() {
-	std::cin.tie(0) -> sync_with_stdio(0);
+        std::cin.tie(0) -> sync_with_stdio(0);
 
-	auto solve = [&]() -> void {
+        auto solve = [&]() -> void {
 
-	};
+        };
 
-	int testcases = 1; std::cin >> testcases;
-	for (int i = 1; i <= testcases; ++ i) {
-		solve();
-	}
+        int testcases = 1; std::cin >> testcases;
+        for (int i = 1; i <= testcases; ++ i) {
+                solve();
+        }
+
+        return 0;
 }
